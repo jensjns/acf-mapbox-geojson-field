3fa0c75a83f12ac1741402124e75b62117f75c1b
Jens Nilsson <jens.nilsson@24hr.se>
2015-04-20 22:17:14 +0200

    mapbox geojson field

---
+++ js/input.js
@@ -0,0 +1,127 @@
+(function($){
+
+    function initialize_field( $el ) {
+
+        var mapDOM = $el.find('.mapbox-geojson-map');
+        var inputField = $el.find('.mapbox-geojson-field');
+
+        var settings = {
+            accessToken: mapDOM.attr('data-access-token'),
+            mapId: mapDOM.attr('data-map-id'),
+        };
+
+        var jsonData = null;
+
+        try {
+            var existingData = JSON.parse($(inputField).val());
+            jsonData = existingData
+        } catch (err) {}
+
+        if( settings.accessToken && settings.mapId ) {
+
+            var map = L.mapbox.map(mapDOM[0], settings.mapId, settings).setView([40, -74.50], 9);
+
+             //L.featureGroup().addTo(map);
+
+            var editableLayer = L.mapbox.featureLayer(jsonData, settings).addTo(map);
+
+            var drawControl = new L.Control.Draw({
+                draw: {
+                    polyline: {
+                        shapeOptions: {
+                            color: '#000000',
+                        },
+                    },
+                    polygon: {
+                        shapeOptions: {
+                            color: '#000000',
+                        },
+                    },
+                    circle: false,
+                    rectangle: {
+                        shapeOptions: {
+                            color: '#000000',
+                        },
+                    },
+                    marker: true,
+                },
+                edit: {
+                    featureGroup: editableLayer,
+                },
+            }).addTo(map);
+
+            map.on('draw:created', function(e) {
+                    editableLayer.addLayer(e.layer);
+                })
+                .on('draw:created draw:edited draw:deleted', function(e) {
+                    $(inputField).val(JSON.stringify(editableLayer.toGeoJSON()));
+                });
+        }
+        else {
+            console.log('mapbox-geojson-err-1');
+        }
+
+    }
+
+
+    if( typeof acf.add_action !== 'undefined' ) {
+
+        /*
+        *  ready append (ACF5)
+        *
+        *  These are 2 events which are fired during the page load
+        *  ready = on page load similar to $(document).ready()
+        *  append = on new DOM elements appended via repeater field
+        *
+        *  @type    event
+        *  @date    20/07/13
+        *
+        *  @param   $el (jQuery selection) the jQuery element which contains the ACF fields
+        *  @return  n/a
+        */
+
+        acf.add_action('ready append', function( $el ){
+
+            // search $el for fields of type 'mapbox_geojson'
+            acf.get_fields({ type : 'mapbox_geojson'}, $el).each(function(){
+
+                initialize_field( $(this) );
+
+            });
+
+        });
+
+
+    } else {
+
+
+        /*
+        *  acf/setup_fields (ACF4)
+        *
+        *  This event is triggered when ACF adds any new elements to the DOM.
+        *
+        *  @type    function
+        *  @since   1.0.0
+        *  @date    01/01/12
+        *
+        *  @param   event       e: an event object. This can be ignored
+        *  @param   Element     postbox: An element which contains the new HTML
+        *
+        *  @return  n/a
+        */
+
+        $(document).on('acf/setup_fields', function(e, postbox){
+
+            $(postbox).find('.field[data-field_type="mapbox_geojson"]').each(function(){
+
+                initialize_field( $(this) );
+
+            });
+
+        });
+
+
+    }
+
+
+})(jQuery);
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Advanced Custom Fields: MapBox geoJSON Field ===
+Contributors: jensnilsson
+Tags: map, mapbox, map-markers, mapping, geojson
+Requires at least: 3.5
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a map field
+
+== Description ==
+
+Adds a map field
+
+= Compatibility =
+
+This ACF field type is compatible with:
+* ACF 5
+* ACF 4
+
+== Installation ==
+
+1. Copy the `acf-mapbox_geojson` folder into your `wp-content/plugins` folder
+2. Activate the MapBox geoJSON plugin via the plugins admin page
+3. Create a new field via ACF and select the MapBox geoJSON type
+4. Please refer to the description for more info regarding the field type settings
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release.
